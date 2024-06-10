3ec44c29caf04a771db33a3700b2bfd0a88f8587
SandeepPuri4 <sndpp21@gmail.com>
2024-06-10 12:31:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html,"],
+  theme: {
+    extend: {
+      color:{
+        primary: "#E6C744",
+      }
+    },
+  },
+  plugins: [],
+}
+
